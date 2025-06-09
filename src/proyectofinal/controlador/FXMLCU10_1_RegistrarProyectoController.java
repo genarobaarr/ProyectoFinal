38ca1b8d1ef54e8db69f4b799b8da79c10ef4122
genarobaarr <genaelcrack0409@gmail.com>
2025-06-08 22:21:37 -0600

    Últimos cambios antes de que los colaboradores clonen

--- src/proyectofinal/controlador/FXMLCU10_1_RegistrarProyectoController.java
+++ src/proyectofinal/controlador/FXMLCU10_1_RegistrarProyectoController.java
@@ -5,29 +5,50 @@
 package proyectofinal.controlador;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import proyectofinal.modelo.pojo.OrganizacionVinculada;
+import proyectofinal.utilidades.Utilidad;
 
 public class FXMLCU10_1_RegistrarProyectoController implements Initializable {
 
     @FXML
-    private TableView<?> tvOrganizacionesVinculadas;
+    private TableView<OrganizacionVinculada> tvOrganizacionesVinculadas;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
 
-
     @FXML
     private void btnCancelar(ActionEvent event) {
+        Alert confirmacion = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmacion.setTitle("Confirmación");
+        confirmacion.setHeaderText("¿Deseas salir?");
+        
+        ButtonType botonSi = new ButtonType("Sí");
+        ButtonType botonNo = new ButtonType("No");
+        confirmacion.getButtonTypes().setAll(botonSi, botonNo);
+        
+        Optional<ButtonType> resultado = confirmacion.showAndWait();
+
+        if (resultado.isPresent() && resultado.get() == botonSi) {
+            cerrarVentana();
+        }
     }
 
     @FXML
     private void btnAceptar(ActionEvent event) {
     }
     
+    private void cerrarVentana(){
+        ((Stage) tvOrganizacionesVinculadas.getScene().getWindow()).close();
+    }
 }
